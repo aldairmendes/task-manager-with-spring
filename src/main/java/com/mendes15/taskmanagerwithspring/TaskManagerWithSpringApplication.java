9992fa81eae7f2e297dca0b89494f4780af8b7ac
mendes15 <aldairryan15@gmail.com>
2026-09-13 11:52:35 -0300

    initial commit

---
+++ src/main/java/com/mendes15/taskmanagerwithspring/TaskManagerWithSpringApplication.java
@@ -0,0 +1,13 @@
+package com.mendes15.taskmanagerwithspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskManagerWithSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TaskManagerWithSpringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mendes15/taskmanagerwithspring/TaskManagerWithSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mendes15.taskmanagerwithspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskManagerWithSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

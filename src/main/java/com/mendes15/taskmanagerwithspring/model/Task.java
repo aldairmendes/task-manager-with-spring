424a29b0b10c38da63fc6276a76aa25ab904da96
mendes15 <aldairryan15@gmail.com>
2026-09-13 11:55:29 -0300

    feat(model): create the models User and Task

---
+++ src/main/java/com/mendes15/taskmanagerwithspring/model/Task.java
@@ -0,0 +1,27 @@
+package com.mendes15.taskmanagerwithspring.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "tasks")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Task {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(nullable = false)
+    private String title;
+
+    private String content;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+}

b741bccde8700a74179f852876dfbc60d7e46819
mendes15 <aldairryan15@gmail.com>
2026-09-20 13:09:51 -0300

    refactor(repository): add useful query methods

--- src/main/java/com/mendes15/taskmanagerwithspring/repository/TaskRepository.java
+++ src/main/java/com/mendes15/taskmanagerwithspring/repository/TaskRepository.java
@@ -1,9 +1,13 @@
 package com.mendes15.taskmanagerwithspring.repository;
 
 import com.mendes15.taskmanagerwithspring.model.Task;
+import com.mendes15.taskmanagerwithspring.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface TaskRepository extends JpaRepository<Task, Integer> {
+    List<Task> findByUser(User user);
 }
--- src/main/java/com/mendes15/taskmanagerwithspring/repository/UserRepository.java
+++ src/main/java/com/mendes15/taskmanagerwithspring/repository/UserRepository.java
@@ -4,6 +4,11 @@ import com.mendes15.taskmanagerwithspring.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
+
 @Repository
 public interface UserRepository extends JpaRepository<User, Integer> {
+    Optional<User> findByUsername(String username);
+    Optional<User> findByEmail(String email);
 }
